d2c62bcf7e14d8851a000cf694c36d9c59a74c43
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-28 15:35:52 +0530

    Create lower_or_upper.cpp

---
+++ lower_or_upper.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	cin>>ch;
+	if(ch>=97&&ch<=122)
+	{
+		cout<<"lowercase";
+	}
+	else
+	{
+		cout<<"uppercase";
+	}
+
+	return 0;
+}
