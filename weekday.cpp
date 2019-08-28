959b23be64d266054b5c405ac64df2cd2e11aef9
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-28 15:40:33 +0530

    Create weekday.cpp

---
+++ weekday.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int week;
+	cin>>week;
+	if(week==1)
+	{
+		cout<<"monday";
+	}
+	else if(week==2)
+	{
+		cout<<"tuesday";
+	}
+	else if(week==3)
+	{
+		cout<<"wednesday";
+	}
+	else if(week==4)
+	{
+		cout<<"thursday";
+	}
+	else if(week==5)
+	{
+		cout<<"friday";
+	}
+	else if(week==6)
+	{
+		cout<<"saturday";
+	}
+	else if(week==7)
+	{
+		cout<<"sunday";
+	}
+	else
+	{
+		cout<<"invalid";
+	}
+	return 0;
+}
