7831a9b2d1a68cecd41aefffaf17a041d8975c68
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-28 14:33:29 +0530

    Create arithmeticopertions.cpp

---
+++ arithmeticopertions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c,d,e,f,g;
+	cin>>a>>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	cout<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g;
+	return 0;
+}
