5ce82bb57addf8f7925eaa2931955aebaa473b50
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-28 15:19:19 +0530

    Create pos_neg_zero.cpp

---
+++ pos_neg_zero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0)
+	{
+		cout<<"positive";
+	}
+	else if(n<0)
+	{
+		cout<<"negative";
+	}
+	else
+	{
+		cout<<"zero";
+	}
+	return 0;
+}
